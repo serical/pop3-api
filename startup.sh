4f195584112e7a565d42b0c0408470a45bbb5fc1
serical <raoserical@gmail.com>
2022-01-19 01:02:31 +0800

    启动脚本

---
+++ startup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+password=$1
+if [ -z "${password}" ]; then
+  echo "请输入接口认证密码"
+  exit 1
+fi
+
+yum install -y java-1.8.0-openjdk
+yum install -y psmisc
+
+killall java
+
+yum install -y git
+yum install -y maven
+
+git clone https://github.com/serical/pop3-api /root
+cd /root/pop3-api && mvn -DskipTests=true package && (nohup java -jar target/pop3-api-0.0.1-SNAPSHOT.jar --password="$password" &) || exit
